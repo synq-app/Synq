218956216aa9f4dde20f784aaef797685bd526c2
stefanie <stefaniebaarman@gmail.com>
2025-03-23 15:47:02 -0400

    adding env vars

--- src/screens/FirstTimeUser/firebaseConfig.tsx
+++ src/screens/FirstTimeUser/firebaseConfig.tsx
@@ -2,16 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPhoneNumber, signInWithEmailAndPassword, createUserWithEmailAndPassword, initializeAuth } from "firebase/auth"; // Import necessary Firebase Auth methods
 import 'firebase/firestore';
 import { getStorage } from "firebase/storage";
+import { ENV_VARS } from '../../../config';
 
 
 const firebaseConfig = {
-  apiKey: "todo",
-  authDomain: "todo",
-  projectId: "todo",
-  storageBucket: "todo",
-  messagingSenderId: "todo",
-  appId: "todo",
-  measurementId: "todo"
+  apiKey: ENV_VARS.apiKey ,
+  authDomain: ENV_VARS.authDomain,
+  projectId: ENV_VARS.projectId,
+  storageBucket: ENV_VARS.storageBucket,
+  messagingSenderId: ENV_VARS.messagingSenderId,
+  appId: ENV_VARS.appId,
+  measurementId: ENV_VARS.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
